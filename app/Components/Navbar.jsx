b863a6c977593be460108de421a0ef875f515e7c
Ali Mohamed <ali@Alis-MacBook-Pro.local>
2024-07-03 11:41:15 +0300

    dark/light mode done

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,8 +1,9 @@
 'use client';
 import React from 'react';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@components/ui/Button';
 import { useRouter } from 'next/navigation';
 import { github } from '../utils/icons';
+import ThemeDropdown from './ThemeDropdown/ThemeDropdown';
 
 const Navbar = () => {
 	const router = useRouter();
@@ -10,6 +11,7 @@ const Navbar = () => {
 		<div className='w-full py-4 flex items-center justify-between'>
 			<div className='left'></div>
 			<div className='search-container flex shrink-0 w-full gap-2 sm:w-fit'>
+				<ThemeDropdown />
 				<Button
 					className='source-code flex items-center gap-2'
 					onClick={() => {
